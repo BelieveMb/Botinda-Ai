4138642355d26818b6ca7d5a85217d83c3476b09
Giovanni <mbolokobelieve@gmail.com>
2025-12-05 05:39:24 +0100

    first commit of new repo

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+import Homepage from './pages/Homepage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+         <div style={{ flex: 1, overflow: 'auto' }}>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              {/* <Route path="/login" element={<Login />} />
+              <Route path="/verify-code" element={<VerifyCode />} />
+              <Route path="/signup" element={<Register />} />
+              <Route path="/dashboard" element={<DashboardPage />} /> 
+              <Route path="/order/new" element={<NewOrder />} /> 
+              <Route path="/order/1" element={<OrderDetail />} />  */}
+            </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,28 @@
+import { MainButton } from "../components/smallComponents/mainButton";
+
+export default function Homepage() {
+
+
+
+
+
+
+
+  return (
+    <section className="w-full layoutMain ">
+      <aside className="w-full lg:w-xl flex flex-col justify-center items-center">
+        <div className="my-40 flex flex-col justify-center items-center content-center">
+          <img src="./../../public/logo.png" alt="logo Botinda,"  className="w-60  lg:w-80 h-52 lg:h-60" />
+          <h1 class="text-2xl lg:text-3xl font-semibold tracking-tight text-balance text-white sm:text-7xl text-center -mt-7 w-full lg:w-[28rem]">Je géres tes commandes pendant que tu dors.</h1>
+
+        </div>
+        <MainButton title="👉 Commencez gratuitement" />
+      </aside>
+
+      <div className="my-8 lg:my-6 flex justify-center content-center">
+        <p>© BoTinda 2025 | Contact | CGU</p>
+      </div>
+
+    </section>
+  );
+}
